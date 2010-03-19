8b9a1a26254e806c10fb30001bacb872e5f5d223
Lukas Botsch <lukas.botsch@gmail.com>
2010-03-19 01:38:03 +0100

    Put global configuration in separate file admin/config.php

--- admin/library-mgr.php
+++ admin/library-mgr.php
@@ -2,9 +2,10 @@
 
 // TODO: Check for user authenticity!!!
 
+require_once 'config.php';
 require_once 'lib/file-mgr.class.php';
 
-$mgr = new FileManager("/EXCHANGE/Music", "library");
+$mgr = new FileManager(LIBRARY_PATH, LIBRARY_ROOT);
 $mgr->handleRequest($_REQUEST);
 $mgr->sendResponse();
 
--- admin/playlist-mgr.php
+++ admin/playlist-mgr.php
@@ -2,10 +2,11 @@
 
 // TODO: Check for user authenticity!!!
 
+require_once 'config.php';
 require_once 'lib/playlist-mgr.class.php';
 require_once 'lib/db.class.php';
 
-Db::open("localhost", "root", "janvier", "radioman");
+Db::open(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 $mgr = new PlaylistManager("/EXCHANGE/Music", "library");
 $mgr->handleRequest($_REQUEST);
 $mgr->sendResponse();
